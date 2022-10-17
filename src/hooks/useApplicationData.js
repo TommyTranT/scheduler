645c017d6ac1b7e652fce21588ebdecac0674d6d
Tommy Tran <haitran1995@gmail.com>
2022-10-17 03:23:03 -0400

    Add useApplicationData File

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+import {useEffect, useState } from "react";
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  useEffect(() => {
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+    });
+  
+  }, [])
+
+  function bookInterview(id, interview) {
+
+    const days = state.days.map(day => {
+      if (day.appointments.includes(id)) {
+        if (!state.appointments[id].interview) {
+          return {...day, spots: day.spots - 1}
+        }
+      }
+      return day;
+    })
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.put(`/api/appointments/${id}`, {interview})
+    .then(() => {
+      setState(prev => ({...prev, appointments, days}));
+    })
+
+  }
+
+  const cancelInterview = (id) => {
+
+    const days = state.days.map(day => {
+      if (day.appointments.includes(id)) {
+        if (state.appointments[id].interview) {
+          return {...day, spots: day.spots + 1}
+        }
+      }
+      return day;
+    })
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: null
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    return axios.delete(`/api/appointments/${id}`, {interview: null})
+    .then(() => {
+      setState(prev => ({...prev, appointments, days}));
+    })
+  }
+
+  const setDay = day => setState({ ...state, day });
+
+  return {
+    state,
+    bookInterview,
+    cancelInterview,
+    setDay, 
+    setState
+  }
+}
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function useVisualMode(initialMode) {
+  const [history, setHistory] = useState([initialMode]);
+
+  const transition = (newView, replace = false) => {
+
+    if (replace) {
+      return setHistory((prev) => {
+        const copy = [
+          ...prev.slice(0, prev.length - 1),
+          newView
+        ];
+
+        return copy;
+      });
+    }
+
+    setHistory((prev) => {
+      const copy = [...prev, newView];
+      return copy;
+    });
+  };
+
+  const back = () => {
+    setHistory((prev) => {
+      if (prev.length > 1) {
+        const copy = prev.slice(0, prev.length - 1);
+        return copy;
+      }
+
+      return prev;
+    });
+  };
+
+  return {
+    mode: history[history.length - 1],
+    transition,
+    back
+  };
+};
