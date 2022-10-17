3ecc33f464d6275b7a250317651324d070c16ffa
Tommy Tran <haitran1995@gmail.com>
2022-10-17 04:02:57 -0400

    Fix testing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,51 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import axios from "axios";
+
+import { render, cleanup, getByText, getAllByTestId, getByAltText, queryByText, fireEvent, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday"));
+  });
+
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+
+    const { getByText } = render(<Application />);
+  
+    await waitForElement(() => getByText("Monday"))   
+
+    fireEvent.click(getByText("Tuesday"));
+
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  
+  });
+
+  it('shows the delete error when failing to delete an existing appointment', async () => {
+
+    axios.delete.mockRejectedValueOnce(new Error('error'));
+
+    const { container } = render(<Application />);
+
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+    
+    const appointment = getAllByTestId(container, 'appointment').find(
+      (appointment) => queryByText(appointment, 'Archie Cohen')
+    );
+    
+    fireEvent.click(getByAltText(appointment, 'Delete'));
+    fireEvent.click(getByText(appointment, 'Confirm'));
+
+    await waitForElement(() => getByText(appointment, 'Error'));
+    expect(getByText(appointment, 'Unable to delete')).toBeInTheDocument();
+
+  });
 });
